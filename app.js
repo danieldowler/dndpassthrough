ca5a858f72f7539120bde4b2449fcb16b0cad5d4
Daniel Dowler <shaolinsamurai1988@gmail.com>
2018-02-19 18:39:00 -0700

    heroku update

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var dnd = require('./routes/dnd');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const dnd = require('./routes/dnd');
 
-var app = express();
+const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', index);
 app.use('/users', users);
@@ -29,7 +30,7 @@ app.use('/dnd', dnd);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
--- routes/dnd.js
+++ routes/dnd.js
@@ -2,15 +2,34 @@ var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({message:"Hello"});
 });
 
 router.get('/races', (req, response) => {
-  fetch('http://www.dnd5eapi.co/api/races')
+  fetch('https://www.dnd5eapi.co/api/races')
       .then(res => res.json())
       .then(races => response.json(races))
+      .catch(err=>{
+        console.log(err);
+        res.status(500).end();
+      });
+});
+
+router.get('/skills', (req, response) => {
+  fetch('https://www.dnd5eapi.co/api/skills')
+      .then(res => res.json())
+      .then(skills => response.json(skills))
+      .catch(err=>{
+        console.log(err);
+        res.status(500).end();
+      });
+});
+
+router.get('/abilities', (req, response) => {
+  fetch('https://www.dnd5eapi.co/api/ability-scores')
+      .then(res => res.json())
+      .then(abilities => response.json(abilities))
       .catch(err=>{
         console.log(err);
         res.status(500).end();
