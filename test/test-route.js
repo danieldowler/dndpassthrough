6dc53fc03a49281538fbc9719aa15db537dccb43
daniel dowler <shaolinsamurai1988@gmail.com>
2018-02-21 18:56:38 -0700

    integration test success?

---
+++ test/test-route.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const expect =  chai.expect;
+
+describe('dndRouter', () => {
+it('should do something when tested', () => {
+    expect(true).to.be.equal(true)
+});
+
+})
